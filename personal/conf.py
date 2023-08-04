0ef3e3c29bfd5dfe756e277ad7c537a6a6b6501c
Beatrueman <1783172311@qq.com>
2023-08-04 19:22:09 +0800

    fix:new conf

--- personal/conf.py
+++ personal/conf.py
@@ -1,2 +1,2 @@
-WEBHOOK_URL = "https://open.feishu.cn/open-apis/bot/v2/hook/706d4867-99bc-4ec8-8c38-e7e39b20bb6d"  #你的机器人的webhook
-CITY = "北京"  #对该城市进行天气监测，注意不要带市和区。如: 青岛、铁西
+WEBHOOK_URL = ""  #你的机器人的webhook
+CITY = ""  #对该城市进行天气监测，注意不要带市和区。如: 青岛、铁西
--- uploadimg.py
+++ uploadimg.py
@@ -4,20 +4,20 @@ from requests_toolbelt import MultipartEncoder
 
 #获取tenant_access_token
 url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/"
-post_data = {"app_id":"cli_a42011585561100d",
-             "app_secret":"AYRFbDlUH8OKxRweuXM47cLLFwRpO12X"}
+post_data = {"app_id":"",
+             "app_secret":""}
 r = requests.post(url, data=post_data)
 tat = r.json()["tenant_access_token"]
 
 def uploadimage():
     url = "https://open.feishu.cn/open-apis/im/v1/images"
-    form = {'image_type':'message','image':(open(r'C:\Users\李逸雄\Desktop\寒潮.png','rb'))} #rb表示用二进制模式只读图片
+    form = {'image_type':'message','image':(open(r'','rb'))} #rb表示用二进制模式只读图片，open后填写图片路径
     multi_form = MultipartEncoder(form)
     headers = {
         'Authorization': 'Bearer '+tat #获取tenant_access_token
     }
     headers['Content-Type'] = multi_form.content_type
-    response = requests.request("POST", url, headers=headers, data=multi_form)
+    response = requests.request("POST", url, headers=headers, data=form)
     print(response.content)
 
 if __name__ == '__main__':
